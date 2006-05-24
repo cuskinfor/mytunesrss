730ba5f8247e5e7ca8b72f476c28c36b90d9c9c1
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2006-05-24 23:07:33 +0000

    tuning
    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1029 6cd82e91-2702-0410-863d-86aa3370aca5

--- Runtime/src/de/codewave/mytunesrss/datastore/ITunesUtils.java
+++ Runtime/src/de/codewave/mytunesrss/datastore/ITunesUtils.java
@@ -40,9 +40,10 @@ public class ITunesUtils {
 
     public static void loadFromITunes(URL iTunesLibraryXml, DataStoreSession storeSession) throws SQLException {
         try {
-            Map plist = (Map)XmlUtils.parseApplePList(iTunesLibraryXml);
-            createTracks(plist, storeSession);
-            createPlaylists(plist, storeSession);
+            PListHandler handler = new PListHandler();
+            handler.addListener("/plist/dict[Tracks]/dict", new TrackListener(storeSession));
+            handler.addListener("/plist/dict[Playlists]/array", new PlaylistListener(storeSession));
+            XmlUtils.parseApplePList(iTunesLibraryXml, handler);
         } catch (ParserConfigurationException e) {
             if (LOG.isErrorEnabled()) {
                 LOG.error("Could not read data from iTunes xml file.", e);
@@ -58,71 +59,99 @@ public class ITunesUtils {
         }
     }
 
-    private static void createTracks(Map plist, DataStoreSession storeSession) throws SQLException {
-        Map<String, Map<String, String>> tracks = (Map<String, Map<String, String>>)plist.get("Tracks");
-        for (Iterator<Map<String, String>> trackIterator = tracks.values().iterator(); trackIterator.hasNext();) {
-            Map<String, String> track = trackIterator.next();
-            insertTrack(track, storeSession);
-            trackIterator.remove();// done with this track, free up some memory
-        }
-    }
+    public static class TrackListener implements PListHandlerListener {
+        DataStoreSession myDataStoreSession;
 
-    private static void insertTrack(Map<String, String> track, DataStoreSession storeSession) throws SQLException {
-        String trackId = track.get("Track ID");
-        String name = track.get("Name");
-        String trackType = track.get("Track Type");
-        if ("File".equals(trackType)) {
-            File file = ITunesUtils.getFileForLocation(track.get("Location"));
-            if (StringUtils.isNotEmpty(trackId) && StringUtils.isNotEmpty(name) && file != null) {
-                try {
-                    InsertTrackStatement statement = new InsertTrackStatement();
-                    statement.setId(trackId.trim());
-                    statement.setName(name.trim());
-                    statement.setArtist(StringUtils.trimToNull(track.get("Artist")));
-                    statement.setAlbum(StringUtils.trimToNull(track.get("Album")));
-                    statement.setTime(StringUtils.isNotEmpty(track.get("Total Time")) ? Integer.parseInt(track.get("Total Time")) / 1000 : 0);
-                    statement.setTrackNumber(StringUtils.isNotEmpty(track.get("Track Number")) ? Integer.parseInt(track.get("Track Number")) : 0);
-                    statement.setFileName(file != null ? file.getAbsolutePath() : null);
-                    storeSession.executeStatement(statement);
-                } catch (NumberFormatException e) {
-                    if (LOG.isErrorEnabled()) {
-                        LOG.error("Could not insert track \"" + name + "\" into database", e);
+        public TrackListener(DataStoreSession dataStoreSession) {
+            myDataStoreSession = dataStoreSession;
+        }
+
+        public boolean beforeDictPut(Map dict, String key, Object value) {
+            insertTrack((Map)value);
+            return false;
+        }
+
+        public boolean beforeArrayAdd(List array, Object value) {
+            throw new UnsupportedOperationException("method beforeArrayAdd of class ITunesUtils$TrackListener is not supported!");
+        }
+
+        private void insertTrack(Map track) {
+            Integer trackId = (Integer)track.get("Track ID");
+            String name = (String)track.get("Name");
+            String trackType = (String)track.get("Track Type");
+            if ("File".equals(trackType)) {
+                File file = ITunesUtils.getFileForLocation((String)track.get("Location"));
+                if (trackId != null && StringUtils.isNotEmpty(name) && file != null) {
+                    try {
+                        InsertTrackStatement statement = new InsertTrackStatement();
+                        statement.setId(trackId.toString());
+                        statement.setName(name.trim());
+                        statement.setArtist(StringUtils.trimToNull((String)track.get("Artist")));
+                        statement.setAlbum(StringUtils.trimToNull((String)track.get("Album")));
+                        statement.setTime(track.get("Total Time") != null ? (Integer)track.get("Total Time") / 1000 : 0);
+                        statement.setTrackNumber(track.get("Track Number") != null ? (Integer)track.get("Track Number") : 0);
+                        statement.setFileName(file != null ? file.getAbsolutePath() : null);
+                        myDataStoreSession.executeStatement(statement);
+                    } catch (SQLException e) {
+                        if (LOG.isErrorEnabled()) {
+                            LOG.error("Could not insert track \"" + name + "\" into database", e);
+                        }
+                    } catch (NumberFormatException e) {
+                        if (LOG.isErrorEnabled()) {
+                            LOG.error("Could not insert track \"" + name + "\" into database", e);
+                        }
                     }
                 }
             }
         }
     }
 
-    private static void createPlaylists(Map plist, DataStoreSession storeSession) throws SQLException {
-        List<Map<String, Object>> playlists = (List<Map<String, Object>>)plist.get("Playlists");
-        for (Iterator<Map<String, Object>> iterator = playlists.iterator(); iterator.hasNext();) {
-            Map<String, Object> playlist = iterator.next();
-            if (!Boolean.TRUE.equals(playlist.get("Master"))) {// ignore master list
-                insertPlaylist(playlist, storeSession);
-            }
-            iterator.remove();// done with this playlist, free up some memory
-        }
-    }
+    public static class PlaylistListener implements PListHandlerListener {
+        DataStoreSession myDataStoreSession;
 
-    private static void insertPlaylist(Map<String, Object> playlist, DataStoreSession storeSession) throws SQLException {
-        String id = (String)playlist.get("Playlist ID");
-        String name = (String)playlist.get("Name");
-
-        List<Map<String, String>> items = (List<Map<String, String>>)playlist.get("Playlist Items");
-        List<String> tracks = new ArrayList<String>();
-        if (items != null && !items.isEmpty()) {
-            for (Iterator<Map<String, String>> itemIterator = items.iterator(); itemIterator.hasNext();) {
-                Map<String, String> item = itemIterator.next();
-                tracks.add(item.get("Track ID"));
-            }
+        public PlaylistListener(DataStoreSession dataStoreSession) {
+            myDataStoreSession = dataStoreSession;
         }
 
-        if (!tracks.isEmpty()) {
-            InsertPlaylistStatement statement = new InsertITunesPlaylistStatement();
-            statement.setId(id);
-            statement.setName(name);
-            statement.setTrackIds(tracks);
-            storeSession.executeStatement(statement);
+        public boolean beforeDictPut(Map dict, String key, Object value) {
+            throw new UnsupportedOperationException("method beforeDictPut of class ITunesUtils$PlaylistListener is not supported!");
+        }
+
+        public boolean beforeArrayAdd(List array, Object value) {
+            insertPlaylist((Map)value);
+            return false;
+        }
+
+        private void insertPlaylist(Map playlist) {
+            boolean master = (Boolean)playlist.get("Master") != null && ((Boolean)playlist.get("Master")).booleanValue();
+            boolean purchased = (Boolean)playlist.get("Purchased Music") != null && ((Boolean)playlist.get("Purchased Music")).booleanValue();
+            boolean partyShuffle = (Boolean)playlist.get("Party Shuffle") != null && ((Boolean)playlist.get("Party Shuffle")).booleanValue();
+
+            if (!master && !purchased && !partyShuffle) {
+                Integer id = (Integer)playlist.get("Playlist ID");
+                String name = (String)playlist.get("Name");
+                List<Map> items = (List<Map>)playlist.get("Playlist Items");
+                List<String> tracks = new ArrayList<String>();
+                if (items != null && !items.isEmpty()) {
+                    for (Iterator<Map> itemIterator = items.iterator(); itemIterator.hasNext();) {
+                        Map item = itemIterator.next();
+                        tracks.add(item.get("Track ID").toString());
+                    }
+                }
+                if (!tracks.isEmpty()) {
+                    InsertPlaylistStatement statement = new InsertITunesPlaylistStatement();
+                    statement.setId(id.toString());
+                    statement.setName(name);
+                    statement.setTrackIds(tracks);
+                    try {
+                        myDataStoreSession.executeStatement(statement);
+                    } catch (SQLException e) {
+                        if (LOG.isErrorEnabled()) {
+                            LOG.error("Could not insert playlist \"" + name + "\" into database.", e);
+                        }
+                    }
+                }
+            }
         }
     }
 }
