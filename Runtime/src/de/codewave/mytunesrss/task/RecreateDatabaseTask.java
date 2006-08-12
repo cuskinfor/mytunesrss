3cd379fab081c0d1381bde1297c139c2fccabfe1
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2006-08-12 06:58:59 +0000

    BETA signature
    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1280 6cd82e91-2702-0410-863d-86aa3370aca5

--- Runtime/src/de/codewave/mytunesrss/task/RecreateDatabaseTask.java
+++ Runtime/src/de/codewave/mytunesrss/task/RecreateDatabaseTask.java
@@ -7,17 +7,13 @@ package de.codewave.mytunesrss.task;
 import de.codewave.mytunesrss.*;
 import de.codewave.mytunesrss.datastore.*;
 import de.codewave.utils.*;
-import org.apache.commons.logging.*;
 
-import javax.swing.*;
 import java.io.*;
 
 /**
  * de.codewave.mytunesrss.task.InitializeDatabaseTask
  */
 public class RecreateDatabaseTask extends InitializeDatabaseTask {
-    private static final Log LOG = LogFactory.getLog(RecreateDatabaseTask.class);
-
     public void execute() throws IOException {
         MyTunesRss.STORE.destroy();
         String pathname = null;
