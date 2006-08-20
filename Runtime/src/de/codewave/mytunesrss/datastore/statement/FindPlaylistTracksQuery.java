99138e1a24620deec13680343155c34fbf6dff0e
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2006-08-20 11:13:44 +0000

    bugfix: splitted playlist queries were incorrect
    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1305 6cd82e91-2702-0410-863d-86aa3370aca5

--- Runtime/src/de/codewave/mytunesrss/datastore/statement/FindPlaylistTracksQuery.java
+++ Runtime/src/de/codewave/mytunesrss/datastore/statement/FindPlaylistTracksQuery.java
@@ -16,47 +16,35 @@ import java.util.*;
  */
 public class FindPlaylistTracksQuery extends DataStoreQuery<Collection<Track>> {
     private static final Log LOG = LogFactory.getLog(FindPlaylistTracksQuery.class);
-    
+
     private static final String BASE_SQL =
             "SELECT ltp.index AS index, t.id AS id, t.name AS name, t.artist AS artist, t.album AS album, t.time AS time, t.track_number AS track_number, t.file AS file, t.protected AS protected, t.video AS video FROM link_track_playlist ltp, track t WHERE t.id = ltp.track_id AND ltp.playlist_id = ?";
-    private static final String LIMIT = " LIMIT ? OFFSET ?";
-    private static final String PLAYLIST_ORDER = BASE_SQL + " ORDER BY index";
-    private static final String PLAYLIST_ORDER_WITH_LIMIT = PLAYLIST_ORDER + LIMIT;
-    private static final String ALBUM_ORDER = BASE_SQL + " ORDER BY album, track_number, name";
-    private static final String ALBUM_ORDER_WITH_LIMIT = ALBUM_ORDER + LIMIT;
-    private static final String ARTIST_ORDER = BASE_SQL + " ORDER BY artist, album, track_number, name";
-    private static final String ARTIST_ORDER_WITH_LIMIT = ARTIST_ORDER + LIMIT;
+    private static final String BASE_SQL_WITH_LIMIT = BASE_SQL + " AND ltp.index >= ? AND ltp.index <= ?";
+
+    private static final String ORDER_PLAYLIST = " ORDER BY index";
+    private static final String ORDER_ALBUM = " ORDER BY album, track_number, name";
+    private static final String ORDER_ARTIST = " ORDER BY artist, album, track_number, name";
+
+    private static final String QUERY_PLAYLIST_ORDER = BASE_SQL + ORDER_PLAYLIST;
+    private static final String QUERY_LIMITED_PLAYLIST_ORDER = BASE_SQL_WITH_LIMIT + ORDER_PLAYLIST;
+    private static final String QUERY_ALBUM_ORDER = BASE_SQL + ORDER_ALBUM;
+    private static final String QUERY_LIMITED_ALBUM_ORDER = BASE_SQL_WITH_LIMIT + ORDER_ALBUM;;
+    private static final String QUERY_ARTIST_ORDER = BASE_SQL + ORDER_ARTIST;
+    private static final String QUERY_LIMITED_ARTIST_ORDER = BASE_SQL_WITH_LIMIT + ORDER_ARTIST;
+
     private FindPlaylistTracksQuery.TrackResultBuilder myBuilder = new FindPlaylistTracksQuery.TrackResultBuilder();
     private String mySql;
     private Object[] myParameters;
 
     public FindPlaylistTracksQuery(String id) {
-        myParameters = getParameters(id);
-        mySql = myParameters.length == 3 ? PLAYLIST_ORDER_WITH_LIMIT : PLAYLIST_ORDER;
-    }
-
-    private String[] getParameters(String id) {
-        String[] splitId = StringUtils.split(id, "_");
-        if (splitId.length != 3) {
-            return splitId;
-        }
-        try {
-            String[] params = new String[3];
-            params[0] = splitId[0];
-            params[1] = Integer.toString(Integer.parseInt(splitId[2]) - Integer.parseInt(splitId[1]) + 1);
-            params[2] = splitId[1];
-            return params;
-        } catch (NumberFormatException e) {
-            if (LOG.isWarnEnabled()) {
-                LOG.warn("Could not parse playlist ID parameters.");
-            }
-            return splitId;
-        }
+        myParameters = StringUtils.split(id, "_");
+        mySql = myParameters.length == 3 ? QUERY_LIMITED_PLAYLIST_ORDER : QUERY_PLAYLIST_ORDER;
     }
 
     public FindPlaylistTracksQuery(String id, boolean sortByArtist) {
-        myParameters = getParameters(id);
-        mySql = sortByArtist ? (myParameters.length == 3 ? ARTIST_ORDER_WITH_LIMIT : ARTIST_ORDER) : (myParameters.length == 3 ? ALBUM_ORDER_WITH_LIMIT : ALBUM_ORDER);
+        myParameters = StringUtils.split(id, "_");
+        mySql = sortByArtist ? (myParameters.length == 3 ? QUERY_LIMITED_ARTIST_ORDER : QUERY_ARTIST_ORDER) : (myParameters.length == 3 ?
+                QUERY_LIMITED_ALBUM_ORDER : QUERY_ALBUM_ORDER);
     }
 
     public Collection<Track> execute(Connection connection) throws SQLException {
