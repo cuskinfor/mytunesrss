7fb329c7c2ddf25c9b2347f6126c3743704d42fe
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2006-11-30 17:43:20 +0000

    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1561 6cd82e91-2702-0410-863d-86aa3370aca5

---
+++ Runtime/src/de/codewave/mytunesrss/MyTunesRssComUpdateTask.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2006, Codewave Software. All Rights Reserved.
+ */
+
+package de.codewave.mytunesrss;
+
+import de.codewave.utils.*;
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+import org.apache.commons.logging.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * de.codewave.mytunesrss.DatabaseWatchdogTask
+ */
+public class MyTunesRssComUpdateTask extends TimerTask {
+    private static final Log LOG = LogFactory.getLog(MyTunesRssComUpdateTask.class);
+    private static final String MYTUNESRSSCOM_URL = "http://www.mytunesrss.com/save_ip.php";
+
+    private int myInterval;
+    private Timer myTimer;
+    private String myUsername;
+    private byte[] myPasswordHash;
+
+    public MyTunesRssComUpdateTask(Timer timer, int interval, String username, byte[] passwordHash) {
+        myTimer = timer;
+        myInterval = interval;
+        myUsername = username;
+        myPasswordHash = passwordHash;
+    }
+
+    public void run() {
+        String base64Hash = Base64Utils.encode(myPasswordHash);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Updating mytunesrss.com: user=\"" + myUsername + "\", password=\"" + base64Hash + "\", port=\"" +
+                    MyTunesRss.CONFIG.getPort() + "\".");
+        }
+        PostMethod postMethod = new PostMethod(System.getProperty("MyTunesRSS.mytunesrsscomUrl", MYTUNESRSSCOM_URL));
+        postMethod.addParameter("user", myUsername);
+        postMethod.addParameter("pass", base64Hash);
+        postMethod.addParameter("port", Integer.toString(MyTunesRss.CONFIG.getPort()));
+        HttpClient client = new HttpClient();
+        try {
+            client.executeMethod(postMethod);
+        } catch (IOException e) {
+            if (LOG.isInfoEnabled()) {
+                LOG.info("Could not update mytunesrss.com", e);
+            }
+        }
+        try {
+            myTimer.schedule(new MyTunesRssComUpdateTask(myTimer, myInterval, myUsername, myPasswordHash), myInterval);
+        } catch (IllegalStateException e) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Could not re-schedule task!", e);
+            }
+        }
+    }
+}
