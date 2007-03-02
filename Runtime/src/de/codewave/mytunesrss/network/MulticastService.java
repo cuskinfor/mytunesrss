c8b56db76f9766d968c1239777a4e1a5ffa25334
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2007-03-02 07:49:50 +0000

    bugfixes
    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1689 6cd82e91-2702-0410-863d-86aa3370aca5

--- Runtime/src/de/codewave/mytunesrss/network/MulticastService.java
+++ Runtime/src/de/codewave/mytunesrss/network/MulticastService.java
@@ -70,7 +70,8 @@ public class MulticastService extends Thread {
                     socket.send(sendPacket);
                 }
                 try {
-                    Collection<String> localAddresses = Arrays.asList(ServerInfo.getLocalAddresses(String.valueOf(MyTunesRss.CONFIG.getPort())));
+                    Collection<String> localAddresses = new ArrayList(Arrays.asList(ServerInfo.getLocalAddresses(String.valueOf(MyTunesRss.CONFIG.getPort()))));
+                    localAddresses.add("http://127.0.0.1:" + MyTunesRss.CONFIG.getPort());
                     socket.setSoTimeout(2000);
                     buffer = new byte[1024];
                     DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
@@ -79,9 +80,8 @@ public class MulticastService extends Thread {
                         String answer = new String(receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8");
                         String name = answer.substring(answer.indexOf(':') + 1);
                         int port = Integer.parseInt(answer.substring(0, answer.indexOf(':')));
-                        String address = receivePacket.getAddress().getHostName();
-                        if (!localAddresses.contains("http://" + address + ":" + port)) {
-                            otherInstances.add(new RemoteServer(name, address, port));
+                      if (!localAddresses.contains("http://" + receivePacket.getAddress().getHostAddress() + ":" + port)) {
+                            otherInstances.add(new RemoteServer(name, receivePacket.getAddress().getHostName(), port));
                         }
                     }
                 } catch (SocketTimeoutException e) {
@@ -121,7 +121,7 @@ public class MulticastService extends Thread {
                     // intentionally left blank
                 } catch (IOException e) {
                     if (LOG.isErrorEnabled()) {
-                        LOG.error("Could not send multicas answer.", e);
+                        LOG.error("Could not send multicast answer.", e);
                     }
                 }
             }
