fbc46151a14b5868fb4ab040e13d35db8f97c59a
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2007-04-24 21:50:40 +0000

    sql queries without help tables (as comments)
    git-svn-id: svn://127.0.0.1/MyTunesRss/trunk@1783 6cd82e91-2702-0410-863d-86aa3370aca5

--- Runtime/src/de/codewave/mytunesrss/datastore/statement/FindAlbumQuery.java
+++ Runtime/src/de/codewave/mytunesrss/datastore/statement/FindAlbumQuery.java
@@ -46,10 +46,26 @@ public class FindAlbumQuery extends DataStoreQuery<Collection<Album>> {
         if (StringUtils.isNotEmpty(myArtist)) {
             statement = connection.prepareStatement(
                     "SELECT name, track_count, artist_count, artist FROM album WHERE name IN ( SELECT DISTINCT(album) FROM track WHERE artist = ? ) ORDER BY name");
+            /** same statement without help table (album)
+             SELECT t1.album, count(distinct(t2.id)), count(distinct(t2.artist)), CASEWHEN(count(distinct(t2.artist)) = 1, GROUP_CONCAT(distinct(t2.artist)), '!')
+             FROM track t1
+             INNER JOIN track t2 ON t1.album = t2.album
+             WHERE t1.artist = ?
+             GROUP BY t1.album
+             ORDER BY t1.album
+             */
             return execute(statement, myBuilder, myArtist);
         } else if (StringUtils.isNotEmpty(myGenre)) {
             statement = connection.prepareStatement(
                     "SELECT name, track_count, artist_count, artist FROM album WHERE name IN ( SELECT DISTINCT(album) FROM track WHERE genre = ? ) ORDER BY name");
+            /** same statement without help table (album)
+             SELECT t1.album, count(distinct(t2.id)), count(distinct(t2.artist)), CASEWHEN(count(distinct(t2.artist)) = 1, GROUP_CONCAT(distinct(t2.artist)), '!')
+             FROM track t1
+             INNER JOIN track t2 ON t1.album = t2.album
+             WHERE t1.genre = ?
+             GROUP BY t1.album
+             ORDER BY t1.album
+             */
             return execute(statement, myBuilder, myGenre);
         } else if (myIndex > -1) {
             ResultSet resultSet = connection.createStatement().executeQuery(
@@ -57,9 +73,24 @@ public class FindAlbumQuery extends DataStoreQuery<Collection<Album>> {
             resultSet.next();
             statement = connection.prepareStatement("SELECT name, track_count, artist_count, artist FROM album WHERE " + resultSet.getString(
                     "CONDITION") + " ORDER BY name");
+            /**
+             SELECT t1.album, count(distinct(t2.id)), count(distinct(t2.artist)), CASEWHEN(count(distinct(t2.artist)) = 1, GROUP_CONCAT(distinct(t2.artist)), '!')
+             FROM track t1
+             INNER JOIN track t2 ON t1.album = t2.album
+             WHERE lower(substring(ltrim(t1.album), 0, 1)) >= ? AND lower(substring(ltrim(t1.album), 0, 1)) <= ?
+             GROUP BY t1.album
+             ORDER BY t1.album
+             */
             return execute(statement, myBuilder);
         } else {
             statement = connection.prepareStatement("SELECT name, track_count, artist_count, artist FROM album ORDER BY name");
+            /**
+             SELECT t1.album, count(distinct(t2.id)), count(distinct(t2.artist)), CASEWHEN(count(distinct(t2.artist)) = 1, GROUP_CONCAT(distinct(t2.artist)), '!')
+             FROM track t1
+             INNER JOIN track t2 ON t1.album = t2.album
+             GROUP BY t1.album
+             ORDER BY t1.album
+             */
             return execute(statement, myBuilder);
         }
     }
