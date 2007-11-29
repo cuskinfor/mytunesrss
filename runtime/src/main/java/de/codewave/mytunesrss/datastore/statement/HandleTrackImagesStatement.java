fa69e4e49c117bd47310e7c104b4d28342ff481f
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2007-11-29 20:26:12 +0000

    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@2596 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/datastore/statement/HandleTrackImagesStatement.java
+++ runtime/src/main/java/de/codewave/mytunesrss/datastore/statement/HandleTrackImagesStatement.java
@@ -41,11 +41,17 @@ public class HandleTrackImagesStatement implements DataStoreStatement {
                 }
                 boolean existing = new FindTrackImageQuery(myTrackId, 32).execute(connection) != null;
                 if (image != null  && image.getData() != null && !existing) {
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Original image size is " + image.getData().length + " bytes.");
+                    }
                     new InsertImageStatement(myTrackId, 32, ImageUtils.resizeImageWithMaxSize(image.getData(), 32)).execute(connection);
                     new InsertImageStatement(myTrackId, 64, ImageUtils.resizeImageWithMaxSize(image.getData(), 64)).execute(connection);
                     new InsertImageStatement(myTrackId, 128, ImageUtils.resizeImageWithMaxSize(image.getData(), 128)).execute(connection);
                     new InsertImageStatement(myTrackId, 256, ImageUtils.resizeImageWithMaxSize(image.getData(), 256)).execute(connection);
                 } else if (image != null && image.getData() != null && existing) {
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Original image size is " + image.getData().length + " bytes.");
+                    }
                     new UpdateImageStatement(myTrackId, 32, ImageUtils.resizeImageWithMaxSize(image.getData(), 32)).execute(connection);
                     new UpdateImageStatement(myTrackId, 64, ImageUtils.resizeImageWithMaxSize(image.getData(), 64)).execute(connection);
                     new UpdateImageStatement(myTrackId, 128, ImageUtils.resizeImageWithMaxSize(image.getData(), 128)).execute(connection);
