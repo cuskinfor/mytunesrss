3dbdf924435b0186ad3c5f3c3cdb6ca13404b446
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2008-05-31 19:21:01 +0000

    Remote-API: PlaylistService
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@3120 6cd82e91-2702-0410-863d-86aa3370aca5

--- webapp/src/main/java/de/codewave/mytunesrss/remote/Session.java
+++ webapp/src/main/java/de/codewave/mytunesrss/remote/Session.java
@@ -1,6 +1,12 @@
 package de.codewave.mytunesrss.remote;
 
 import de.codewave.mytunesrss.User;
+import de.codewave.mytunesrss.datastore.statement.Playlist;
+import de.codewave.mytunesrss.datastore.statement.Track;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * de.codewave.mytunesrss.remote.Session
@@ -10,6 +16,7 @@ public class Session {
     private User myUser;
     private int myTimeout;
     private long myExpiration;
+    private Map<String, Object> myAttributes = new HashMap<String, Object>();
 
     public Session(String id, User user, int timeout) {
         myId = id;
@@ -38,5 +45,17 @@ public class Session {
 
     public User getUser() {
         return myUser;
+    }
+
+    public Object setAttribute(String key, Object value) {
+        return myAttributes.put(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return myAttributes.get(key);
+    }
+
+    public Object removeAttribute(String key) {
+        return myAttributes.remove(key);
     }
 }
--- webapp/src/main/java/de/codewave/mytunesrss/remote/service/PlaylistService.java
+++ webapp/src/main/java/de/codewave/mytunesrss/remote/service/PlaylistService.java
@@ -2,19 +2,27 @@ package de.codewave.mytunesrss.remote.service;
 
 import de.codewave.mytunesrss.User;
 import de.codewave.mytunesrss.command.MyTunesRssCommand;
-import de.codewave.mytunesrss.datastore.statement.FindPlaylistQuery;
+import de.codewave.mytunesrss.datastore.statement.*;
 import de.codewave.mytunesrss.remote.MyTunesRssRemoteEnv;
+import de.codewave.mytunesrss.remote.Session;
 import de.codewave.mytunesrss.remote.render.RenderMachine;
 import de.codewave.mytunesrss.servlet.TransactionFilter;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Service for playlist retrieval and management.
  */
 public class PlaylistService {
+    // todo: remote-api: testing
+    private static final Log LOG = LogFactory.getLog(PlaylistService.class);
+    private static final String KEY_EDIT_PLAYLIST = "editPlaylist";
+    private static final String KEY_EDIT_PLAYLIST_TRACKS = "editPlaylistTracks";
+
     /**
      * Get all visible playlist from the database.
      *
@@ -71,66 +79,228 @@ public class PlaylistService {
         return MyTunesRssRemoteEnv.getServerCall(MyTunesRssCommand.CreateRss, "playlist=" + playlistId);
     }
 
-    public String startEditPlaylist(String playlistId) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    /**
+     * Start editing a playlist. If an ID is specified it has be to the ID of a MyTunesRSS playlist.
+     *
+     * @param playlistId The ID of the playlist to edit or <code>null</code> to start an empty playlist.
+     *
+     * @return The playlist and track list.
+     *
+     * @throws IllegalAccessException   Unauthorized.
+     * @throws SQLException             Could not execute SQL to find playlist.
+     * @throws IllegalArgumentException Playlist with specified ID not found.
+     */
+    public Object startEditPlaylist(String playlistId) throws IllegalAccessException, SQLException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = new Playlist();
+            Collection<Track> tracks = new ArrayList<Track>();
+            if (StringUtils.isNotEmpty(playlistId)) {
+                FindPlaylistQuery query = new FindPlaylistQuery(user, PlaylistType.MyTunes, playlistId, true, true);
+                List<Playlist> queryResult = TransactionFilter.getTransaction().executeQuery(query).getResults();
+                if (queryResult != null && queryResult.size() == 1) {
+                    playlist = queryResult.get(0);
+                    tracks = new LinkedHashSet<Track>(TransactionFilter.getTransaction().executeQuery(new FindPlaylistTracksQuery(user,
+                                                                                                                                  playlistId,
+                                                                                                                                  null)).getResults());
+                } else {
+                    throw new IllegalArgumentException("Playlist not found");
+                }
+            }
+            session.setAttribute(KEY_EDIT_PLAYLIST, playlist);
+            session.setAttribute(KEY_EDIT_PLAYLIST_TRACKS, tracks);
+            return getPlaylistAndTracksRenderResult(playlist, tracks);
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
-    public void addTrackToPlaylist(String trackId) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    /**
+     * Create the render result for a playlist and the playlist tracks.
+     *
+     * @param playlist The playlist.
+     * @param tracks   The tracks of the playlist.
+     *
+     * @return The render result.
+     */
+    private Object getPlaylistAndTracksRenderResult(Playlist playlist, Collection<Track> tracks) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("playlist", playlist);
+        map.put("tracks", tracks);
+        return RenderMachine.getInstance().render(map);
+    }
+
+    /**
+     * Add a new track to the currently edited playlist.
+     *
+     * @param trackId The ID of the track to add.
+     *
+     * @return The playlist and list of tracks after adding the new track.
+     *
+     * @throws IllegalAccessException
+     * @throws SQLException
+     */
+    public Object addTrackToPlaylist(String trackId) throws IllegalAccessException, SQLException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                if (playlist != null) {
+                    List<Track> tracks = TransactionFilter.getTransaction().executeQuery(FindTrackQuery.getForId(new String[] {trackId}))
+                            .getResults();
+                    if (tracks != null && !tracks.isEmpty()) {
+                        playlistTracks.add(tracks.get(0));
+                        playlist.setTrackCount(playlistTracks.size());
+                    }
+                }
+                return getPlaylistAndTracksRenderResult(playlist, playlistTracks);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
-    public void addTracksToPlaylist(String[] trackIds) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    /**
+     * Add a number of tracks to the currently edited playlist.
+     *
+     * @param trackIds Array of track IDs of tracks to add.
+     *
+     * @return The playlist and list of tracks after adding the new tracks.
+     *
+     * @throws IllegalAccessException
+     * @throws SQLException
+     */
+    public Object addTracksToPlaylist(String[] trackIds) throws IllegalAccessException, SQLException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                if (playlist != null) {
+                    List<Track> tracks = TransactionFilter.getTransaction().executeQuery(FindTrackQuery.getForId(trackIds)).getResults();
+                    if (tracks != null && !tracks.isEmpty()) {
+                        playlistTracks.addAll(tracks);
+                        playlist.setTrackCount(playlistTracks.size());
+                    }
+                }
+                return getPlaylistAndTracksRenderResult(playlist, playlistTracks);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
-    public void removeTrackFromPlaylist(String trackId) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    /**
+     * Remove a track from the currently edited playlist.
+     *
+     * @param trackId The ID of track to remove.
+     *
+     * @return The playlist and list of tracks after removing the track.
+     *
+     * @throws IllegalAccessException
+     */
+    public Object removeTrackFromPlaylist(String trackId) throws IllegalAccessException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                if (playlist != null) {
+                    Track track = new Track();
+                    track.setId(trackId);
+                    playlistTracks.remove(track);
+                    playlist.setTrackCount(playlistTracks.size());
+                }
+                return getPlaylistAndTracksRenderResult(playlist, playlistTracks);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
-    public void removeTracksFromPlaylist(String[] trackId) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    /**
+     * Remove a number of tracks from the currently edited playlist.
+     *
+     * @param trackIds The IDs of the tracks to remove.
+     *
+     * @return The playlist and list of tracks after removing the tracks.
+     *
+     * @throws IllegalAccessException
+     */
+    public Object removeTracksFromPlaylist(String[] trackIds) throws IllegalAccessException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                if (playlist != null) {
+                    for (int i = 0; i < trackIds.length; i++) {
+                        Track track = new Track();
+                        track.setId(trackIds[i]);
+                        playlistTracks.remove(track);
+                    }
+                    playlist.setTrackCount(playlistTracks.size());
+                }
+                return getPlaylistAndTracksRenderResult(playlist, playlistTracks);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
     public void cancelEditPlaylist() throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                session.removeAttribute(KEY_EDIT_PLAYLIST);
+                session.removeAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
-    public String savePlaylist(String playlistName, boolean userPrivate) throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+    public void savePlaylist(String playlistName, boolean userPrivate) throws IllegalAccessException {
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                playlist.setName(playlistName);
+                playlist.setUserPrivate(userPrivate);
+                playlist.setUserOwner(user.getName());
+                // todo remote-api: save playlist
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
 
     public Object getEditPlaylist() throws IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
+        Session session = MyTunesRssRemoteEnv.getSession();
+        User user = session.getUser();
         if (user != null) {
-            // todo remote-api
+            Playlist playlist = (Playlist)session.getAttribute(KEY_EDIT_PLAYLIST);
+            if (playlist != null) {
+                Collection<Track> playlistTracks = (Collection<Track>)session.getAttribute(KEY_EDIT_PLAYLIST_TRACKS);
+                return getPlaylistAndTracksRenderResult(playlist, playlistTracks);
+            } else {
+                throw new IllegalStateException("Not currently editing a playlist.");
+            }
         }
         throw new IllegalAccessException("Unauthorized");
     }
