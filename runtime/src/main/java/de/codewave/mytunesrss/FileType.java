8a639708be9a6b58ede0a95758db1dfdbde3b804
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2008-12-31 07:58:32 +0000

    Fixed default type for m4p and mp4 (changed wrong type ion RC-1).
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@3699 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/FileType.java
+++ runtime/src/main/java/de/codewave/mytunesrss/FileType.java
@@ -20,9 +20,9 @@ public class FileType {
     public static List<FileType> getDefaults() {
         List<FileType> types = new ArrayList<FileType>();
         types.add(new FileType(true, "m4a", "audio/x-m4a", false, false));
-        types.add(new FileType(true, "m4p", "video/x-m4p", false, true));
+        types.add(new FileType(true, "m4p", "audio/x-m4p", false, true));
         types.add(new FileType(true, "wav", "audio/wav", false, false));
-        types.add(new FileType(true, "mp4", "audio/x-mp4", false, false));
+        types.add(new FileType(true, "mp4", "video/x-mp4", false, false));
         types.add(new FileType(true, "avi", "video/x-msvideo", true, false));
         types.add(new FileType(true, "mov", "video/quicktime", true, false));
         types.add(new FileType(true, "wmv", "video/x-ms-wmv", true, false));
