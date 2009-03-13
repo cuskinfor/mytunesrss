6084e1afc63ebef24dfcd93e46d978e01c5c83d1
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2009-03-13 22:19:13 +0000

    Remote control of Video Lan Client player through remote API.
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@3936 6cd82e91-2702-0410-863d-86aa3370aca5

--- webapp/src/main/java/de/codewave/mytunesrss/remote/service/VideoLanClientService.java
+++ webapp/src/main/java/de/codewave/mytunesrss/remote/service/VideoLanClientService.java
@@ -9,31 +9,76 @@ import java.io.IOException;
  * de.codewave.mytunesrss.remote.service.VideoLanClientService
  */
 public class VideoLanClientService {
-    private static VideoLanClientProcess myVlcProcess = new VideoLanClientProcess();
+    private static VideoLanClient myVideoLanClient = new VideoLanClient();
 
-    public void start() throws IOException, InterruptedException, IllegalAccessException {
+    private void assertAuthenticated() throws IllegalAccessException {
         User user = MyTunesRssRemoteEnv.getSession().getUser();
-        if (user != null) {
-            myVlcProcess.start();
-        } else {
+        if (user == null) {
             throw new IllegalAccessException("Unauthorized");
         }
     }
 
-    public void stop() throws InterruptedException, IllegalAccessException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
-        if (user != null) {
-            myVlcProcess.stop();
+
+    public void connect(String host, int port) throws IOException, InterruptedException, IllegalAccessException {
+        assertAuthenticated();
+        myVideoLanClient.connect(host, port);
+    }
+
+    public void disconnect() throws InterruptedException, IllegalAccessException, IOException {
+        assertAuthenticated();
+        myVideoLanClient.disconnect();
+    }
+
+    public String add(String item, boolean stop) throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        if (stop) {
+            return myVideoLanClient.sendCommands("add " + item, "stop");
         } else {
-            throw new IllegalAccessException("Unauthorized");
+            return myVideoLanClient.sendCommands("add " + item);
         }
+    }
+
+    public String enqueue(String item, boolean stop) throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        if (stop) {
+            return myVideoLanClient.sendCommands("enqueue " + item, "stop");
+        } else {
+            return myVideoLanClient.sendCommands("enqueue " + item);
+        }
+    }
+
+    public String clear() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("clear");
+    }
+
+    public String play() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("play");
+    }
+
+    public String pause() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("pause");
+    }
+
+    public String stop() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("stop");
+    }
+
+    public String next() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("next");
+    }
+
+    public String prev() throws IllegalAccessException, IOException, InterruptedException {
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands("prev");
     }
 
     public String sendCommand(String command) throws IOException, IllegalAccessException, InterruptedException {
-        User user = MyTunesRssRemoteEnv.getSession().getUser();
-        if (user != null) {
-            return myVlcProcess.sendCommand(command);
-        }
-        throw new IllegalAccessException("Unauthorized");
+        assertAuthenticated();
+        return myVideoLanClient.sendCommands(command);
     }
 }
