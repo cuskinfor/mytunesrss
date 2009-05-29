f1254e048d446f8a559b39fac00375479a2dd9b6
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2009-05-29 22:29:57 +0000

    video playback in window works now
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@4149 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/quicktime/QuicktimePlayer.java
+++ runtime/src/main/java/de/codewave/mytunesrss/quicktime/QuicktimePlayer.java
@@ -20,6 +20,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.awt.*;
 
 /**
  * de.codewave.mytunesrss.quicktime.QuicktimePlayer
