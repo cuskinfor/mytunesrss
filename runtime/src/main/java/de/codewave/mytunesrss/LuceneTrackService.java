bf4123694e4e6ffe6da828769ca12765c2fac3be
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2009-06-17 16:17:07 +0000

    some modifications
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@4231 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/LuceneTrackService.java
+++ runtime/src/main/java/de/codewave/mytunesrss/LuceneTrackService.java
@@ -1,18 +1,19 @@
 package de.codewave.mytunesrss;
 
 import de.codewave.mytunesrss.datastore.statement.FindPlaylistTracksQuery;
-import de.codewave.mytunesrss.datastore.statement.Track;
 import de.codewave.mytunesrss.datastore.statement.SortOrder;
+import de.codewave.mytunesrss.datastore.statement.Track;
 import de.codewave.utils.PrefsUtils;
 import de.codewave.utils.sql.DataStoreQuery;
 import de.codewave.utils.sql.DataStoreSession;
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -29,6 +30,12 @@ import java.util.*;
 public class LuceneTrackService {
     private static final Logger LOGGER = LoggerFactory.getLogger(LuceneTrackService.class);
 
+    private static final Set<String> STOP_WORDS = new HashSet<String>();
+
+    {
+        STOP_WORDS.addAll(StopFilter.makeStopSet(StopAnalyzer.ENGLISH_STOP_WORDS));
+    }
+
     private Directory getDirectory() throws IOException {
         return FSDirectory.getDirectory(PrefsUtils.getCacheDataPath(MyTunesRss.APPLICATION_IDENTIFIER) + "/lucene/track");
     }
@@ -37,7 +44,8 @@ public class LuceneTrackService {
         LOGGER.debug("Indexing all tracks.");
         long start = System.currentTimeMillis();
         Directory directory = getDirectory();
-        IndexWriter iwriter = new IndexWriter(directory, new StandardAnalyzer(Collections.emptySet()), true, new IndexWriter.MaxFieldLength(300));
+        StandardAnalyzer analyzer = new StandardAnalyzer(STOP_WORDS);
+        IndexWriter iwriter = new IndexWriter(directory, analyzer, true, new IndexWriter.MaxFieldLength(300));
         FindPlaylistTracksQuery query = new FindPlaylistTracksQuery(FindPlaylistTracksQuery.PSEUDO_ID_ALL_BY_ALBUM, SortOrder.KeepOrder);
         DataStoreSession session = MyTunesRss.STORE.getTransaction();
         DataStoreQuery.QueryResult<Track> queryResult = session.executeQuery(query);
@@ -56,45 +64,15 @@ public class LuceneTrackService {
         LOGGER.debug("Finished indexing all tracks (duration: " + (System.currentTimeMillis() - start) + " ms).");
     }
 
-/*
-    public void indexTrack(String id, String name, String album, String artist) throws IOException {
-        LOGGER.debug("Index track.");
-        Directory directory = getDirectory();
-        IndexWriter iwriter = new IndexWriter(directory, new StandardAnalyzer(), new IndexWriter.MaxFieldLength(2000));
-        Document document = new Document();
-        document.add(new Field("id", id, Field.Store.YES, Field.Index.NO));
-        document.add(new Field("name", name, Field.Store.NO, Field.Index.ANALYZED));
-        document.add(new Field("album", album, Field.Store.NO, Field.Index.ANALYZED));
-        document.add(new Field("artist", artist, Field.Store.NO, Field.Index.ANALYZED));
-        iwriter.addDocument(document);
-        iwriter.optimize();
-        iwriter.close();
-        directory.close();
-    }
-
-    public void removeTracksFromIndex(String... ids) throws IOException {
-        LOGGER.debug("Removed tracks from index.");
-        Directory directory = getDirectory();
-        IndexWriter iwriter = new IndexWriter(directory, new StandardAnalyzer(), new IndexWriter.MaxFieldLength(2000));
-        for (String id : ids) {
-            iwriter.deleteDocuments(new Term("id", id));
-        }
-        iwriter.optimize();
-        iwriter.close();
-        directory.close();
-    }
-*/
-
     public List<String> searchTrackIds(String[] searchTerms, boolean fuzzy) throws IOException, ParseException {
         Directory directory = getDirectory();
         final IndexSearcher isearcher = new IndexSearcher(directory);
-        QueryParser parser = new QueryParser("name", new StandardAnalyzer(Collections.emptySet()));
-        Query luceneQuery = parser.parse(createQueryString(searchTerms, fuzzy));
+        Query luceneQuery = createQuery(searchTerms);
         final BitSet bits = new BitSet();
         isearcher.search(luceneQuery, new HitCollector() {
             @Override
             public void collect(int i, float v) {
-                    bits.set(i);
+                bits.set(i);
             }
         });
         final List<String> trackIds = new ArrayList<String>();
@@ -106,20 +84,17 @@ public class LuceneTrackService {
         return trackIds;
     }
 
-    private String createQueryString(String[] searchTerms, boolean fuzzy) {
-        StringBuilder queryString = new StringBuilder();
-        for (String field : new String[]{"name", "album", "artist"}) {
-            queryString.append("(");
-            for (int i = 0; i < searchTerms.length; i++) {
-                queryString.append(field).append(":").append(QueryParser.escape(searchTerms[i])).append(fuzzy ? "~" : "");
-                if (i + 1 < searchTerms.length) {
-                    queryString.append(" AND ");
+    private Query createQuery(String[] searchTerms) {
+        BooleanQuery andQuery = new BooleanQuery();
+        for (String searchTerm : searchTerms) {
+            if (!STOP_WORDS.contains(searchTerm)) {
+                BooleanQuery orQuery = new BooleanQuery();
+                for (String field : new String[]{"name", "album", "artist"}) {
+                    orQuery.add(new FuzzyQuery(new Term(field, searchTerm), 0.5f), BooleanClause.Occur.SHOULD);
                 }
+                andQuery.add(orQuery, BooleanClause.Occur.MUST);
             }
-            queryString.append(") OR ");
         }
-        String result = queryString.substring(0, queryString.length() - 4);
-        LOGGER.debug("Created query string \"" + result + "\".");
-        return result;
+        return andQuery;
     }
 }
