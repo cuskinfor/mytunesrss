e555fd1f78d72f7e92023adfc48e039f9fefe500
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2010-03-27 20:46:25 +0000

    support and registration config panel
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@5303 6cd82e91-2702-0410-863d-86aa3370aca5

--- admin/src/main/java/de/codewave/mytunesrss/webadmin/StatusPanel.java
+++ admin/src/main/java/de/codewave/mytunesrss/webadmin/StatusPanel.java
@@ -142,6 +142,8 @@ public class StatusPanel extends Panel implements Button.ClickListener {
             application.setMainComponent(new StreamingConfigPanel(getApplication(), myComponentFactory));
         } else if (clickEvent.getButton() == myAddonsConfig) {
             application.setMainComponent(new AddonsConfigPanel(getApplication(), myComponentFactory));
+        } else if (clickEvent.getButton() == mySupportConfig) {
+            application.setMainComponent(new SupportConfigPanel(getApplication(), myComponentFactory));
         }
     }
 }
---
+++ admin/src/main/java/de/codewave/mytunesrss/webadmin/SupportConfigPanel.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2010. Codewave Software Michael Descher.
+ * All rights reserved.
+ */
+
+package de.codewave.mytunesrss.webadmin;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+import de.codewave.mytunesrss.MyTunesRss;
+import de.codewave.mytunesrss.MyTunesRssUtils;
+import de.codewave.mytunesrss.task.SendSupportRequestTask;
+import de.codewave.vaadin.ComponentFactory;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Level;
+
+import java.io.OutputStream;
+import java.util.Arrays;
+
+public class SupportConfigPanel extends MyTunesRssConfigPanel implements Upload.Receiver, Upload.SucceededListener, Upload.FailedListener {
+
+    private Form mySupportForm;
+    private Form myRegistrationForm;
+    private Form mySysInfoForm;
+    private Select myLogLevel;
+    private TextField myName;
+    private TextField myEmail;
+    private TextField myDescription;
+    private CheckBox myIncludeItunesXml;
+    private Button mySendSupport;
+    private TextField myRegName;
+    private TextField myExpirationDate;
+    private Upload myUploadLicense;
+    private TextField mySysInfo;
+
+    public SupportConfigPanel(Application application, ComponentFactory componentFactory) {
+        super(application, getBundleString("supportConfigPanel.caption"), componentFactory.createGridLayout(1, 4, true, true), componentFactory);
+    }
+
+    protected void init(Application application) {
+        mySupportForm = getComponentFactory().createForm(null, true);
+        myName = getComponentFactory().createTextField("supportConfigPanel.name");
+        myEmail = getComponentFactory().createTextField("supportConfigPanel.email");
+        myDescription = getComponentFactory().createTextField("supportConfigPanel.description");
+        myDescription.setRows(10);
+        myIncludeItunesXml = getComponentFactory().createCheckBox("supportConfigPanel.includeItunesXml");
+        mySendSupport = getComponentFactory().createButton("supportConfigPanel.sendSupport", this);
+        mySupportForm.addField("logLevel", myLogLevel);
+        mySupportForm.addField("name", myName);
+        mySupportForm.addField("email", myEmail);
+        mySupportForm.addField("description", myDescription);
+        mySupportForm.addField("includeItunesXml", myIncludeItunesXml);
+        mySupportForm.addField("sendSupport", mySendSupport);
+        addComponent(getComponentFactory().surroundWithPanel(mySupportForm, FORM_PANEL_MARGIN_INFO, getBundleString("supportConfigPanel.caption.support")));
+        myRegistrationForm = getComponentFactory().createForm(null, true);
+        myRegName = getComponentFactory().createTextField("supportConfigPanel.regName");
+        myRegName.setEnabled(false);
+        myExpirationDate = getComponentFactory().createTextField("supportConfigPanel.expirationDate");
+        myExpirationDate.setEnabled(false);
+        myUploadLicense = new Upload(null, this);
+        myUploadLicense.setButtonCaption(getBundleString("supportConfigPanel.uploadLicense"));
+        myUploadLicense.setImmediate(true);
+        myUploadLicense.addListener((Upload.SucceededListener)this);
+        myUploadLicense.addListener((Upload.FailedListener)this);
+        myRegistrationForm.addField("regName", myRegName);
+        myRegistrationForm.addField("expirationDate", myExpirationDate);
+        Panel registrationPanel = getComponentFactory().surroundWithPanel(myRegistrationForm, new Layout.MarginInfo(false, true, true, true), getBundleString("supportConfigPanel.caption.registration"));
+        registrationPanel.addComponent(myUploadLicense);
+        addComponent(registrationPanel);
+        mySysInfoForm = getComponentFactory().createForm(null, true);
+        myLogLevel = getComponentFactory().createSelect("supportConfigPanel.logLevel", Arrays.asList(Level.OFF, Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG));
+        mySysInfo = getComponentFactory().createTextField("supportConfigPanel.sysInfo");
+        mySysInfo.setEnabled(false);
+        mySysInfo.setRows(10);
+        mySysInfoForm.addField("logLevel", myLogLevel);
+        mySysInfoForm.addField("sysInfo", mySysInfo);
+        addComponent(getComponentFactory().surroundWithPanel(mySysInfoForm, FORM_PANEL_MARGIN_INFO, getBundleString("supportConfigPanel.caption.sysInfo")));
+
+        addMainButtons(0, 3, 0, 3);
+    }
+
+    protected void initFromConfig(Application application) {
+        myLogLevel.setValue(MyTunesRss.CONFIG.getCodewaveLogLevel());
+        myName.setValue(MyTunesRss.CONFIG.getSupportName());
+        myEmail.setValue(MyTunesRss.CONFIG.getSupportEmail());
+        myRegName.setValue(MyTunesRss.REGISTRATION.getName());
+        if (MyTunesRss.REGISTRATION.isExpirationDate()) {
+            myExpirationDate.setValue(MyTunesRss.REGISTRATION.getExpiration());
+        }
+        mySysInfo.setValue(MyTunesRssUtils.getSystemInfo());
+    }
+
+    protected void writeToConfig() {
+        MyTunesRss.CONFIG.setCodewaveLogLevel((Level) myLogLevel.getValue());
+        MyTunesRss.CONFIG.setSupportName((String) myName.getValue());
+        MyTunesRss.CONFIG.setSupportEmail((String) myEmail.getValue());
+    }
+
+    public void buttonClick(Button.ClickEvent clickEvent) {
+        if (clickEvent.getSource() == mySendSupport) {
+            if (StringUtils.isNotBlank((String)myName.getValue()) && StringUtils.isNotBlank((String)myEmail.getValue()) && StringUtils.isNotBlank((String)myDescription.getValue())) {
+                SendSupportRequestTask requestTask = new SendSupportRequestTask((String)myName.getValue(), (String)myEmail.getValue(), (String)myDescription.getValue() + "\n\n\n", (Boolean)myIncludeItunesXml.getValue());
+                requestTask.execute();
+                if (requestTask.isSuccess()) {
+                    getApplication().showInfo("supportConfigPanel.info.supportRequestSent");
+                } else {
+                    getApplication().showError("supportConfigPanel.error.supportRequestFailed");
+                }
+            } else {
+                getApplication().showError("supportConfigPanel.error.allFieldsMandatoryForSupport");
+            }
+        } else {
+            super.buttonClick(clickEvent);
+        }
+    }
+
+    public OutputStream receiveUpload(String filename, String MIMEType) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void uploadFailed(Upload.FailedEvent event) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void uploadSucceeded(Upload.SucceededEvent event) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
