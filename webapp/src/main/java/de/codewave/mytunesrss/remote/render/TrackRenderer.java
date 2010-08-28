d968f9846fa65157666e166ebaaf54493797ca92
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2010-08-28 12:01:45 +0000

    temporary disabled http live streaming for release
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@5704 6cd82e91-2702-0410-863d-86aa3370aca5

--- webapp/src/main/java/de/codewave/mytunesrss/remote/render/TrackRenderer.java
+++ webapp/src/main/java/de/codewave/mytunesrss/remote/render/TrackRenderer.java
@@ -35,11 +35,11 @@ public class TrackRenderer implements Renderer<Map<String, Object>, Track> {
         result.put("trackNumber", track.getTrackNumber());
         result.put("tsPlayed", track.getTsPlayed());
         result.put("tsUpdated", track.getTsUpdated());
-        if (MyTunesRssWebUtils.getUserAgent(MyTunesRssRemoteEnv.getRequest()) == UserAgent.Iphone && track.getMediaType() == MediaType.Video) {
-            result.put("playbackUrl", MyTunesFunctions.httpLiveStreamUrl(MyTunesRssRemoteEnv.getRequest(), track, null));
-        } else {
+        //if (MyTunesRssWebUtils.getUserAgent(MyTunesRssRemoteEnv.getRequest()) == UserAgent.Iphone && track.getMediaType() == MediaType.Video) {
+        //    result.put("playbackUrl", MyTunesFunctions.httpLiveStreamUrl(MyTunesRssRemoteEnv.getRequest(), track, null));
+        //} else {
             result.put("playbackUrl", MyTunesFunctions.playbackUrl(MyTunesRssRemoteEnv.getRequest(), track, null));
-        }
+        //}
         result.put("downloadUrl", MyTunesFunctions.downloadUrl(MyTunesRssRemoteEnv.getRequest(), track, null));
         result.put("imageUrl", track.getImageHash() != null ? MyTunesRssRemoteEnv.getServerCall(MyTunesRssCommand.ShowImage,
                                                                                              "hash=" + track.getImageHash()) : null);
