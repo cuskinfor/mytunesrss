0dfddc7ce75a07102f6dd20f7546f74eb7ea29c8
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2011-02-24 20:29:37 +0000

    Use same host as admin when wizard finishes instead of 127.0.0.1 which of course only works on the local machine.
    git-svn-id: svn://127.0.0.1/mytunesrss/branches/mytunesrss-4.0.x@6185 6cd82e91-2702-0410-863d-86aa3370aca5

--- admin/src/main/java/de/codewave/mytunesrss/webadmin/WizardWorkingPanel.java
+++ admin/src/main/java/de/codewave/mytunesrss/webadmin/WizardWorkingPanel.java
@@ -6,9 +6,14 @@ import com.vaadin.ui.Panel;
 import de.codewave.mytunesrss.MyTunesRss;
 import de.codewave.vaadin.VaadinUtils;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vaadin.henrik.refresher.Refresher;
+import sun.util.LocaleServiceProviderPool;
 
 public class WizardWorkingPanel extends Panel implements Refresher.RefreshListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WizardWorkingPanel.class);
 
     public void attach() {
         super.attach();
@@ -27,7 +32,11 @@ public class WizardWorkingPanel extends Panel implements Refresher.RefreshListen
 
     public void refresh(Refresher refresher) {
         if (!MyTunesRss.EXECUTOR_SERVICE.isDatabaseUpdateRunning()) {
-            VaadinUtils.getApplicationWindow(this).open(new ExternalResource("http://" + getApplication().getURL().getHost() + ":" + MyTunesRss.CONFIG.getPort() + StringUtils.trimToEmpty(MyTunesRss.CONFIG.getWebappContext())));
+            String url = "http://" + getApplication().getURL().getHost() + ":" + MyTunesRss.CONFIG.getPort() + StringUtils.trimToEmpty(MyTunesRss.CONFIG.getWebappContext());
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("Forwarding to user interface URL \"" + url + "\".");
+            }
+            VaadinUtils.getApplicationWindow(this).open(new ExternalResource(url));
         }
     }
 }
