daa7598ad1dcde5ab38fe80639f333faa3610e57
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2011-09-01 18:43:00 +0000

    config refactorings
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@6522 6cd82e91-2702-0410-863d-86aa3370aca5

--- admin/src/main/java/de/codewave/mytunesrss/webadmin/task/SendSupportRequestTask.java
+++ admin/src/main/java/de/codewave/mytunesrss/webadmin/task/SendSupportRequestTask.java
@@ -5,25 +5,9 @@
 
 package de.codewave.mytunesrss.webadmin.task;
 
-import de.codewave.mytunesrss.DatasourceConfig;
-import de.codewave.mytunesrss.DatasourceType;
-import de.codewave.mytunesrss.MyTunesRss;
-import de.codewave.mytunesrss.MyTunesRssUtils;
 import de.codewave.mytunesrss.task.SendSupportRequestRunnable;
 import de.codewave.mytunesrss.webadmin.MainWindow;
-import de.codewave.utils.io.ZipUtils;
 import de.codewave.vaadin.component.ProgressWindow;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.multipart.*;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.zip.ZipOutputStream;
 
 public class SendSupportRequestTask extends SendSupportRequestRunnable implements ProgressWindow.Task {
     private MainWindow myMainWindow;
