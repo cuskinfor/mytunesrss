e605a15dcbc11ea73fc9ecebb813ae7e0f86cde2
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2012-04-20 23:30:56 +0000

    [MYTUNESRSS-20] Use VLC instead of QTJava for remote control
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@7170 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/vlc/VlcPlayer.java
+++ runtime/src/main/java/de/codewave/mytunesrss/vlc/VlcPlayer.java
@@ -7,15 +7,18 @@ package de.codewave.mytunesrss.vlc;
 
 import de.codewave.mytunesrss.MyTunesRss;
 import de.codewave.mytunesrss.datastore.statement.Track;
+import de.codewave.utils.io.StreamCopyThread;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,51 +29,65 @@ public class VlcPlayer {
 
     private List<Track> myTracks = Collections.emptyList();
 
-    private Process myVlcProcess;
-
     private int myCurrent;
 
-    private boolean myInitialized;
+    private int myOffset;
+
+    private Thread myWatchdog;
+
+    private String myVlcHost = "localhost";
+
+    private int myVlcPort = 18675;
 
     public synchronized void init() throws VlcPlayerException {
-        if (!myInitialized) {
-            LOGGER.debug("Initializing VLC player.");
-            try {
-                ProcessBuilder processBuilder = new ProcessBuilder(
-                        MyTunesRss.CONFIG.getVlcExecutable().getAbsolutePath(),
-                        "--intf=rc"
-                );
-                myVlcProcess = processBuilder.start();
-                new Thread(new Runnable() {
-                    public void run() {
+        if (myWatchdog == null) {
+            myWatchdog = new Thread(new Runnable() {
+                public void run() {
+                    ProcessBuilder processBuilder = new ProcessBuilder(
+                            MyTunesRss.CONFIG.getVlcExecutable().getAbsolutePath(),
+                            "--intf=rc",
+                            "--rc-host=" + myVlcHost + ":" + myVlcPort
+                    );
+                    processBuilder.redirectErrorStream(true);
+                    while (!Thread.interrupted()) {
+                        LOGGER.debug("Initializing VLC player.");
+                        Process process = null;
                         try {
-                            IOUtils.copy(myVlcProcess.getErrorStream(), new NullOutputStream());
+                            process = processBuilder.start();
+                            new StreamCopyThread(process.getInputStream(), false, new NullOutputStream(), true).start();
+                            Thread.sleep(1000);
+                            try {
+                                setVolume(70); // default volume to 70%
+                            } catch (VlcPlayerException e) {
+                                LOGGER.warn("Could not set volume for VLC player.", e);
+                            }
+                            process.waitFor();
                         } catch (IOException e) {
-                            LOGGER.error("Could not consume VLC process error stream.", e);
+                            LOGGER.warn("Could not start VLC player.", e);
+                            break;
+                        } catch (InterruptedException e) {
+                            LOGGER.warn("Interrupted while waiting for process to exit.", e);
+                            break;
+                        } finally {
+                            if (process != null) {
+                                process.destroy();
+                            }
                         }
                     }
-                }).start();
-                receive(TIMEOUT);
-                myInitialized = true;
-                setVolume(70); // default volume to 70%
-            } catch (IOException e) {
-                throw new VlcPlayerException(e);
-            }
+                }
+            });
+            myWatchdog.setDaemon(true);
+            myWatchdog.start();
         }
     }
 
     public synchronized void destroy() throws VlcPlayerException {
-        if (myInitialized) {
-            stop();
-            LOGGER.debug("Destroying VLC player.");
+        if (myWatchdog != null && myWatchdog.isAlive()) {
+            myWatchdog.interrupt();
             try {
-                myVlcProcess.getOutputStream().write("shutdown\r\n".getBytes());
-                receive(TIMEOUT);
-            } catch (IOException e) {
-                LOGGER.error("Could not shutdown VLC player gracefully.", e);
-            } finally {
-                myVlcProcess.destroy();
-                myInitialized = false;
+                myWatchdog.join(5000);
+            } catch (InterruptedException e) {
+                throw new VlcPlayerException("Interrupted while waiting for watchdog thread to exit.", e);
             }
         }
     }
@@ -85,73 +102,85 @@ public class VlcPlayer {
         stop();
         LOGGER.debug("Clearing playlist.");
         myTracks = Collections.emptyList();
-        sendAndReceive("clear");
+        myCurrent = -1;
+        send("clear");
     }
 
     public synchronized void setTracks(List<Track> tracks) throws VlcPlayerException {
+        stop();
         clearPlaylist();
         LOGGER.debug("Setting playlist of " + tracks.size() + " tracks.");
         myTracks = new ArrayList<Track>(tracks);
+        myCurrent = -1;
+        myOffset = -1;
         for (Track track : tracks) {
-            sendAndReceive("enqueue " + track.getFile().getAbsolutePath());
+            send("enqueue " + track.getFile().getAbsolutePath());
+            if (myOffset == -1) {
+                for (String line : StringUtils.split(send("playlist"), "\r\n")) {
+                    int digit = StringUtils.indexOfAny(line, "1234567890");
+                    if (digit > -1 && digit < line.length() - 1) {
+                        int nonDigit = line.indexOf(" ", digit);
+                        if (nonDigit > digit) {
+                            try {
+                                int number = Integer.parseInt(line.substring(digit, nonDigit));
+                                myOffset = Math.max(myOffset, number);
+                            } catch (NumberFormatException e) {
+                                // ignore
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
     public synchronized void setVolume(int volume) throws VlcPlayerException {
-        LOGGER.debug("Setting volume to " + volume + "%.");
         if (volume < 0 || volume > 100) {
             throw new IllegalArgumentException("Volume must be a value from 0 to 100 but was " + volume);
         }
         LOGGER.debug("Setting volume to " + volume + "%.");
-        sendAndReceive("volume " + (volume * 512) / 100);
+        send("volume " + (volume * 512) / 100);
     }
 
-    private String sendAndReceive(String s) throws VlcPlayerException {
-        send(s);
-        return receive(TIMEOUT);
-    }
-
-    private void send(String command) throws VlcPlayerException {
+    private String send(final String command) throws VlcPlayerException {
         try {
-            LOGGER.debug("Sending: " + command);
-            myVlcProcess.getOutputStream().write((command + System.getProperty("line.separator")).getBytes());
-            myVlcProcess.getOutputStream().flush();
+            LOGGER.debug("Sending command: " + command);
+            Socket socket = new Socket(myVlcHost, myVlcPort);
+            receive(socket.getInputStream());
+            socket.getOutputStream().write((command + System.getProperty("line.separator")).getBytes());
+            socket.getOutputStream().flush();
+            String response = receive(socket.getInputStream());
+            socket.getOutputStream().write(("logout" + System.getProperty("line.separator")).getBytes());
+            socket.getOutputStream().flush();
+            IOUtils.copy(socket.getInputStream(), new NullOutputStream());
+            socket.close();
+            LOGGER.debug("Command response: " + response);
+            return response;
         } catch (IOException e) {
-            throw new VlcPlayerException("Could not send command to player.", e);
+            throw new VlcPlayerException("Could not send command \"" + command + "\" to player.", e);
         }
     }
 
-    private String receive(long timeout) throws VlcPlayerException {
-        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Thread readerThread = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    IOUtils.copy(myVlcProcess.getInputStream(), byteArrayOutputStream);
-                } catch (IOException e) {
-                    LOGGER.error("Could not read VLC process output.");
-                }
+    private String receive(InputStream inputStream) throws IOException {
+        StringBuilder responseBuilder = new StringBuilder();
+        byte[] buffer = new byte[8192];
+        for (int read = inputStream.read(buffer); read > 0; read = inputStream.read(buffer)) {
+            responseBuilder.append(new String(buffer, 0, read));
+            if (responseBuilder.toString().endsWith("> ")) {
+                responseBuilder.delete(responseBuilder.length() - 2, responseBuilder.length());
+                break;
             }
-        });
-        readerThread.setDaemon(true);
-        LOGGER.debug("Reading response from VLC process.");
-        readerThread.start();
-        try {
-            readerThread.join(timeout);
-        } catch (InterruptedException e) {
-            throw new VlcPlayerException("Interrupted while receiving response.", e);
         }
-        readerThread.interrupt();
-        String response = new String(byteArrayOutputStream.toByteArray());
-        LOGGER.debug("Received: " + response);
-        return response;
+        LOGGER.trace("Received: " + responseBuilder);
+        return StringUtils.trimToEmpty(responseBuilder.toString());
     }
 
     public synchronized int getVolume() throws VlcPlayerException {
-        String response = sendAndReceive("volume");
+        String response = send("volume");
         try {
             return (Integer.parseInt(response.trim()) * 100) / 512;
         } catch (NumberFormatException e) {
-            LOGGER.warn("Could not parse volume response \"" + response + "\".");
+            LOGGER.info("Could not parse response \"" + response + "\" to current volume.");
             return -1;
         }
     }
@@ -159,13 +188,12 @@ public class VlcPlayer {
     public synchronized void stop() throws VlcPlayerException {
         LOGGER.debug("Stopping playback.");
         setFullScreen(false);
-        sendAndReceive("stop");
-        myCurrent = -1;
+        send("stop");
     }
 
     public synchronized void pause() throws VlcPlayerException {
         LOGGER.debug("Pausing playback.");
-        sendAndReceive("pause");
+        send("pause");
     }
 
     public synchronized void jumpTo(int percentage) throws VlcPlayerException {
@@ -175,7 +203,7 @@ public class VlcPlayer {
         LOGGER.debug("Jumping to position " + percentage + "% of current track.");
         int length = getCurrentTrackLength();
         if (length > 0) {
-            sendAndReceive("seek " + ((length * percentage) / 100));
+            send("seek " + ((length * percentage) / 100));
         }
     }
 
@@ -188,44 +216,57 @@ public class VlcPlayer {
     }
 
     public synchronized boolean isPlaying() throws VlcPlayerException {
-        for (String responseLine : StringUtils.split(sendAndReceive("status"), System.getProperty("line.separator"))) {
-            if (responseLine.contains("state paused") || responseLine.contains("state stopped")) {
-                return false;
-            } else if (responseLine.contains("state playing")) {
-                return true;
-            }
-        }
-        throw new VlcPlayerException("Could not determine play state.");
+        return send("status").contains("state playing");
+    }
+
+    public synchronized boolean isPaused() throws VlcPlayerException {
+        return send("status").contains("state paused");
+    }
+
+    public synchronized boolean isStopped() throws VlcPlayerException {
+        return send("status").contains("state stopped");
     }
 
     public synchronized int getCurrentTime() throws VlcPlayerException {
-        String response = sendAndReceive("get_time");
+        String response = send("get_time");
         try {
             return Integer.parseInt(response);
         } catch (NumberFormatException e) {
-            throw new VlcPlayerException("Could not parse current time response.", e);
+            LOGGER.info("Could not parse response \"" + response + "\" to current time.");
+            return -1;
         }
     }
 
     public synchronized int getCurrentTrackLength() throws VlcPlayerException {
-        String response = sendAndReceive("get_length");
+        String response = send("get_length");
         try {
             return Integer.parseInt(response);
         } catch (NumberFormatException e) {
-            throw new VlcPlayerException("Could not parse length response \"" + response + "\".");
+            LOGGER.info("Could not parse response \"" + response + "\" to current track length.");
+            return -1;
         }
     }
 
     public synchronized boolean setFullScreen(boolean fullScreen) throws VlcPlayerException {
         LOGGER.debug("Switching to " + (fullScreen ? "fullscreen" : "window") + " mode.");
-        sendAndReceive("fullscreen " + (fullScreen ? "on" : "off"));
+        send("fullscreen " + (fullScreen ? "on" : "off"));
         return fullScreen;
     }
 
     public synchronized void play(int index) throws VlcPlayerException {
+        if (index < -1 || index >= myTracks.size()) {
+            throw new IllegalArgumentException("Index out of track array bounds [0, " + (myTracks.size() - 1) + "]: " + index + ".");
+        }
         LOGGER.debug("Playback of track " + index + " requested.");
-        stop();
-        sendAndReceive("goto " + index);
-        sendAndReceive("play");
+        if (index == -1) {
+            if (isPaused()) {
+                send("pause");
+            } else {
+                send("play");
+            }
+        } else {
+            myCurrent = index;
+            send("goto " + (myOffset + index));
+        }
     }
 }
