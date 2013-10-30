29a4a899dbbfa3627afc543208c656580cf48570
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2013-10-30 15:43:11 +0000

    fixed mac os x code signature
    git-svn-id: svn://127.0.0.1/mytunesrss/branches/mytunesrss-4.9.x@8382 6cd82e91-2702-0410-863d-86aa3370aca5

--- etc/macosx-codesign.sh
+++ etc/macosx-codesign.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(find . -wholename ./target/mytunesrss-*-macosx/mytunesrss-*/MyTunesRSS.app | sed -e 's/.*\/mytunesrss-\(.*\)-macosx\/mytunesrss-.*/\1/')
 
-codesign -s "E6450995C010E3E3BD2437C047FBA03856A9CF2D" -f --signature-size 8192 ./target/mytunesrss-${VERSION}-macosx/mytunesrss-${VERSION}/MyTunesRSS.app
+codesign -s "B3A56A04187C1B3BDEABE7B2A377D7A6A17F8151" -f --signature-size 16384 ./target/mytunesrss-${VERSION}-macosx/mytunesrss-${VERSION}/MyTunesRSS.app
 
 pushd ./target/mytunesrss-${VERSION}-macosx
 rm ../mytunesrss-${VERSION}-macosx.zip
