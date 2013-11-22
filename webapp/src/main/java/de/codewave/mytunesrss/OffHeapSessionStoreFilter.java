2c11d5aa2691506f818edf1d54e1f5e45126b10e
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2013-11-22 08:26:50 +0000

    off heap session store for albums
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@8497 6cd82e91-2702-0410-863d-86aa3370aca5

---
+++ webapp/src/main/java/de/codewave/mytunesrss/OffHeapSessionStoreFilter.java
@@ -0,0 +1,28 @@
+package de.codewave.mytunesrss;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class OffHeapSessionStoreFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // nothing to initialize
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            String currentListId = request.getParameter(OffHeapSessionStore.CURRENT_LIST_ID);
+            if (StringUtils.isBlank(currentListId)) {
+                OffHeapSessionStore.get((HttpServletRequest) request).removeCurrentList();
+            }
+
+        }
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        // nothing to destroy
+    }
+}
