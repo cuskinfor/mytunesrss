ac2d5fd4174ac5b69aa9d30a805e4acd2ccbdf9b
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2014-01-29 18:35:58 +0000

    correct code sign cert
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@8628 6cd82e91-2702-0410-863d-86aa3370aca5

--- etc/macosx-codesign.sh
+++ etc/macosx-codesign.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(find . -wholename ./target/mytunesrss-*-macosx/mytunesrss-*/MyTunesRSS.app | sed -e 's/.*\/mytunesrss-\(.*\)-macosx\/mytunesrss-.*/\1/')
 
-codesign -s "B3A56A04187C1B3BDEABE7B2A377D7A6A17F8151" -f --signature-size 16384 ./target/mytunesrss-${VERSION}-macosx/mytunesrss-${VERSION}/MyTunesRSS.app
+codesign -s "Mac Developer: Michael Descher (KA3C22NAMD)" -f --signature-size 16384 --deep ./target/mytunesrss-${VERSION}-macosx/mytunesrss-${VERSION}/MyTunesRSS.app
 
 pushd ./target/mytunesrss-${VERSION}-macosx
 rm ../mytunesrss-${VERSION}-macosx.zip
