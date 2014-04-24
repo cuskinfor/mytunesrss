6d8376a72aa7cba1a6b22121be6a2cbb69e273a3
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2014-04-24 21:38:10 +0000

    Display episode number in episodes list (UPnP)
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@8803 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/mediaserver/TvShowEpisodeDIDL.java
+++ runtime/src/main/java/de/codewave/mytunesrss/mediaserver/TvShowEpisodeDIDL.java
@@ -8,17 +8,21 @@ package de.codewave.mytunesrss.mediaserver;
 import de.codewave.mytunesrss.config.User;
 import de.codewave.mytunesrss.datastore.statement.Track;
 import org.fourthline.cling.support.model.item.Item;
+import org.fourthline.cling.support.model.item.Movie;
 
 public class TvShowEpisodeDIDL extends MyTunesRssItemDIDL {
 
     @Override
     protected Item createItem(Track track, User user, String oidParams) {
-        return createMovieTrack(
+        Movie movie = createMovieTrack(
                 user,
                 track,
                 ObjectID.TvShowEpisode.getValue() + ";" + encode(track.getId()),
                 ObjectID.TvShowSeason.getValue() + ";" + encode(track.getSeries(), Integer.toString(track.getSeason()))
         );
+        // fix title by prepending the episode number
+        movie.setTitle(track.getEpisode() + " - " + mapUnknown(track.getName()));
+        return movie;
     }
 
 }
