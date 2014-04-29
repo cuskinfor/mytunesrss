8c4ec538cf78f31f3de7b627354fc40de4d95004
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2014-04-29 13:11:09 +0000

    [MYTUNESRSS-500] Add FindTrackQueryTests unit tests
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@8842 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/test/java/de/codewave/mytunesrss/datastore/statement/FindTrackQueryTest.java
+++ runtime/src/test/java/de/codewave/mytunesrss/datastore/statement/FindTrackQueryTest.java
@@ -36,6 +36,11 @@ public class FindTrackQueryTest {
             MyTunesRss.STORE.executeQuery(FindTrackQuery.getForArtist(user, new String[]{"ABBA"}, sortOrder));
             MyTunesRss.STORE.executeQuery(FindTrackQuery.getForGenre(user, new String[]{"Pop"}, sortOrder));
             MyTunesRss.STORE.executeQuery(FindTrackQuery.getForSearchTerm(user, "lady gaga", 25, sortOrder, 100));
+            MyTunesRss.STORE.executeQuery(FindTrackQuery.getMovies(user));
+            MyTunesRss.STORE.executeQuery(FindTrackQuery.getTvShowEpisodes(user));
+            MyTunesRss.STORE.executeQuery(FindTrackQuery.getTvShowSeriesEpisodes(user, "Once Upon a Time"));
+            MyTunesRss.STORE.executeQuery(FindTrackQuery.getTvShowSeriesSeasonEpisodes(user, "Once Upon a Time", 1));
+            MyTunesRss.STORE.executeQuery(FindTrackQuery.getForIds(new String[]{"1", "2", "3", "4"}));
         }
     }
 }
