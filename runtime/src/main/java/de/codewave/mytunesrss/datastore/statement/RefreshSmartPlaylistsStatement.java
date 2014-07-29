ec5d53fdca00f4ed81271204744e60191c393536
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2014-07-29 12:07:30 +0000

    Optimized smart playlist refresh by ignoring any refresh requests for a playlist ID while another refresh for the same playlist ID is already waiting to be executed.
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@8988 6cd82e91-2702-0410-863d-86aa3370aca5

--- runtime/src/main/java/de/codewave/mytunesrss/datastore/statement/RefreshSmartPlaylistsStatement.java
+++ runtime/src/main/java/de/codewave/mytunesrss/datastore/statement/RefreshSmartPlaylistsStatement.java
@@ -17,6 +17,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Statement for updating all smart playlists.
@@ -25,7 +27,7 @@ public class RefreshSmartPlaylistsStatement implements DataStoreStatement {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RefreshSmartPlaylistsStatement.class);
     private static final long MILLIS_PER_DAY = 1000L * 3600L * 24L;
-    private static final Object SYNC = new Object();
+    private static final ConcurrentHashMap<String, AtomicInteger> LATCH_MAP = new ConcurrentHashMap<>();
     public enum UpdateType {
         SCHEDULED(), ON_PLAY(), DEFAULT()
     }
@@ -47,6 +49,7 @@ public class RefreshSmartPlaylistsStatement implements DataStoreStatement {
     }
 
     public void execute(Connection connection) throws SQLException {
+        boolean anyPlaylistRefreshed = false;
         if (mySmartInfos == null || mySmartInfos.isEmpty() || StringUtils.isBlank(myPlaylistId)) {
             Collection<SmartPlaylist> smartPlaylists = new DataStoreQuery<Collection<SmartPlaylist>>() {
                 @Override
@@ -59,15 +62,17 @@ public class RefreshSmartPlaylistsStatement implements DataStoreStatement {
             for (SmartPlaylist smartPlaylist : smartPlaylists) {
                 Collection<SmartInfo> smartInfos = smartPlaylist.getSmartInfos();
                 if (myUpdateType == UpdateType.DEFAULT || (myUpdateType == UpdateType.ON_PLAY && isOnPlayRefresh(smartInfos)) || (myUpdateType == UpdateType.SCHEDULED && isScheduledUpdateRefresh(smartInfos))) {
-                    refreshSmartPlaylist(connection, smartInfos, smartPlaylist.getPlaylist().getId());
+                    anyPlaylistRefreshed |= refreshSmartPlaylist(connection, smartInfos, smartPlaylist.getPlaylist().getId());
                     connection.commit();
                 }
             }
         } else {
-            refreshSmartPlaylist(connection, mySmartInfos, myPlaylistId);
+            anyPlaylistRefreshed |= refreshSmartPlaylist(connection, mySmartInfos, myPlaylistId);
         }
-        MyTunesRssEventManager.getInstance().fireEvent(MyTunesRssEvent.create(MyTunesRssEvent.EventType.MEDIA_SERVER_UPDATE));
-        LOGGER.info("Smart playlists have been refreshed.");
+        if (anyPlaylistRefreshed) {
+            MyTunesRssEventManager.getInstance().fireEvent(MyTunesRssEvent.create(MyTunesRssEvent.EventType.MEDIA_SERVER_UPDATE));
+            LOGGER.info("Smart playlists have been refreshed.");
+        }
     }
 
     private boolean isOnPlayRefresh(Collection<SmartInfo> smartInfos) {
@@ -99,144 +104,153 @@ public class RefreshSmartPlaylistsStatement implements DataStoreStatement {
         return result;
     }
 
-    private void refreshSmartPlaylist(Connection connection, Collection<SmartInfo> smartInfos, String playlistId) throws SQLException {
-        LOGGER.debug("Refreshing smart playlist with id \"" + playlistId + "\".");
-        try {
-            if (SmartInfo.isLuceneCriteria(smartInfos)) {
-                Collection<LuceneTrackService.ScoredTrack> scoredTracks = MyTunesRss.LUCENE_TRACK_SERVICE.searchTracks(smartInfos, 0, 10000);
-                MyTunesRssUtils.createStatement(connection, "createSearchTempTables").execute(); // create if not exists
-                MyTunesRssUtils.createStatement(connection, "truncateSearchTempTables").execute(); // truncate if already existed
-                if (!CollectionUtils.isEmpty(scoredTracks)) {
-                    SmartStatement statement = MyTunesRssUtils.createStatement(connection, "fillLuceneSearchTempTable");
-                    statement.setObject("track", toMap(scoredTracks));
-                    statement.execute();
-                }
-            }
-            Map<String, Boolean> conditionals = new HashMap<>();
-            conditionals.put("order_default", true);
-            conditionals.put("lucene", SmartInfo.isLuceneCriteria(smartInfos));
-            conditionals.put("nolucene", !SmartInfo.isLuceneCriteria(smartInfos));
-            for (SmartInfo smartInfo : smartInfos) {
-                switch (smartInfo.getFieldType()) {
-                    case mintime:
-                        conditionals.put("mintime", true);
-                        break;
-                    case datasource:
-                        conditionals.put("datasource", true);
-                        break;
-                    case maxtime:
-                        conditionals.put("maxtime", true);
-                        break;
-                    case mediatype:
-                        conditionals.put("mediatype", true);
-                        break;
-                    case protection:
-                        conditionals.put("protected", true);
-                        break;
-                    case videotype:
-                        conditionals.put("videotype", true);
-                        break;
-                    case recentlyPlayed:
-                        conditionals.put((smartInfo.isInvert() ? "not_" : "") + "recently_played", true);
-                        break;
-                    case recentlyUpdated:
-                        conditionals.put((smartInfo.isInvert() ? "not_" : "") + "recently_updated", true);
-                        break;
-                    case order:
-                        conditionals.put("order_default", false);
-                        switch (SmartOrder.valueOf(smartInfo.getPattern())) {
-                            case random:
-                                conditionals.put("order_random", true);
+    private boolean refreshSmartPlaylist(Connection connection, Collection<SmartInfo> smartInfos, String playlistId) throws SQLException {
+        LATCH_MAP.putIfAbsent(playlistId, new AtomicInteger(0));
+        if (LATCH_MAP.get(playlistId).getAndIncrement() < 2) {
+            synchronized (LATCH_MAP.get(playlistId)) {
+                LOGGER.info("Refreshing smart playlist with id \"" + playlistId + "\".");
+                try {
+                    if (SmartInfo.isLuceneCriteria(smartInfos)) {
+                        Collection<LuceneTrackService.ScoredTrack> scoredTracks = MyTunesRss.LUCENE_TRACK_SERVICE.searchTracks(smartInfos, 0, 10000);
+                        MyTunesRssUtils.createStatement(connection, "createSearchTempTables").execute(); // create if not exists
+                        MyTunesRssUtils.createStatement(connection, "truncateSearchTempTables").execute(); // truncate if already existed
+                        if (!CollectionUtils.isEmpty(scoredTracks)) {
+                            SmartStatement statement = MyTunesRssUtils.createStatement(connection, "fillLuceneSearchTempTable");
+                            statement.setObject("track", toMap(scoredTracks));
+                            statement.execute();
+                        }
+                    }
+                    Map<String, Boolean> conditionals = new HashMap<>();
+                    conditionals.put("order_default", true);
+                    conditionals.put("lucene", SmartInfo.isLuceneCriteria(smartInfos));
+                    conditionals.put("nolucene", !SmartInfo.isLuceneCriteria(smartInfos));
+                    for (SmartInfo smartInfo : smartInfos) {
+                        switch (smartInfo.getFieldType()) {
+                            case mintime:
+                                conditionals.put("mintime", true);
                                 break;
-                            case lastplayed_asc:
-                                conditionals.put("order_lastplayed", true);
+                            case datasource:
+                                conditionals.put("datasource", true);
                                 break;
-                            case lastplayed_desc:
-                                conditionals.put("order_lastplayed", true);
-                                conditionals.put("order_desc", true);
+                            case maxtime:
+                                conditionals.put("maxtime", true);
                                 break;
-                            case lastupdate_asc:
-                                conditionals.put("order_lastupdate", true);
+                            case mediatype:
+                                conditionals.put("mediatype", true);
                                 break;
-                            case lastupdate_desc:
-                                conditionals.put("order_lastupdate", true);
-                                conditionals.put("order_desc", true);
+                            case protection:
+                                conditionals.put("protected", true);
                                 break;
-                            case playcount_asc:
-                                conditionals.put("order_playcount", true);
+                            case videotype:
+                                conditionals.put("videotype", true);
                                 break;
-                            case playcount_desc:
-                                conditionals.put("order_playcount", true);
-                                conditionals.put("order_desc", true);
+                            case recentlyPlayed:
+                                conditionals.put((smartInfo.isInvert() ? "not_" : "") + "recently_played", true);
+                                break;
+                            case recentlyUpdated:
+                                conditionals.put((smartInfo.isInvert() ? "not_" : "") + "recently_updated", true);
+                                break;
+                            case order:
+                                conditionals.put("order_default", false);
+                                switch (SmartOrder.valueOf(smartInfo.getPattern())) {
+                                    case random:
+                                        conditionals.put("order_random", true);
+                                        break;
+                                    case lastplayed_asc:
+                                        conditionals.put("order_lastplayed", true);
+                                        break;
+                                    case lastplayed_desc:
+                                        conditionals.put("order_lastplayed", true);
+                                        conditionals.put("order_desc", true);
+                                        break;
+                                    case lastupdate_asc:
+                                        conditionals.put("order_lastupdate", true);
+                                        break;
+                                    case lastupdate_desc:
+                                        conditionals.put("order_lastupdate", true);
+                                        conditionals.put("order_desc", true);
+                                        break;
+                                    case playcount_asc:
+                                        conditionals.put("order_playcount", true);
+                                        break;
+                                    case playcount_desc:
+                                        conditionals.put("order_playcount", true);
+                                        conditionals.put("order_desc", true);
+                                        break;
+                                    default:
+                                        // no conditionals in other cases
+                                }
+                                break;
+                            case sizeLimit:
+                                conditionals.put("limit", true);
                                 break;
                             default:
                                 // no conditionals in other cases
                         }
-                        break;
-                    case sizeLimit:
-                        conditionals.put("limit", true);
-                        break;
-                    default:
-                        // no conditionals in other cases
-                }
-            }
-            final SmartStatement queryStatement = MyTunesRssUtils.createStatement(connection, "getTracksForSmartPlaylist", conditionals);
-            for (SmartInfo smartInfo : smartInfos) {
-                switch (smartInfo.getFieldType()) {
-                    case mintime:
-                        queryStatement.setInt("time_min", Integer.parseInt(smartInfo.getPattern()));
-                        break;
-                    case datasource:
-                        queryStatement.setString("source_id", smartInfo.getPattern());
-                        break;
-                    case maxtime:
-                        queryStatement.setInt("time_max", Integer.parseInt(smartInfo.getPattern()));
-                        break;
-                    case mediatype:
-                        queryStatement.setString("mediatype", smartInfo.getPattern());
-                        break;
-                    case protection:
-                        queryStatement.setBoolean("protected", Boolean.parseBoolean(smartInfo.getPattern()));
-                        break;
-                    case videotype:
-                        queryStatement.setString("videotype", smartInfo.getPattern());
-                        break;
-                    case sizeLimit:
-                        queryStatement.setInt("maxCount", Integer.parseInt(smartInfo.getPattern()));
-                        break;
-                    case recentlyPlayed:
-                        queryStatement.setLong("ts_played", System.currentTimeMillis() - (MILLIS_PER_DAY * Long.parseLong(smartInfo.getPattern())));
-                        break;
-                    case recentlyUpdated:
-                        queryStatement.setLong("ts_updated", System.currentTimeMillis() - (MILLIS_PER_DAY * Long.parseLong(smartInfo.getPattern())));
-                        break;
-                    default:
-                        // nothing in all other cases
-                }
-            }
-            Collection<String> tracks = new LinkedHashSet<>();
-            DataStoreQuery<QueryResult<String>> dataStoreQuery = new DataStoreQuery<QueryResult<String>>() {
-                @Override
-                public QueryResult<String> execute(Connection connection) throws SQLException {
-                    return execute(queryStatement, new ResultBuilder<String>() {
-                        public String create(ResultSet resultSet) throws SQLException {
-                            return resultSet.getString(1);
+                    }
+                    final SmartStatement queryStatement = MyTunesRssUtils.createStatement(connection, "getTracksForSmartPlaylist", conditionals);
+                    for (SmartInfo smartInfo : smartInfos) {
+                        switch (smartInfo.getFieldType()) {
+                            case mintime:
+                                queryStatement.setInt("time_min", Integer.parseInt(smartInfo.getPattern()));
+                                break;
+                            case datasource:
+                                queryStatement.setString("source_id", smartInfo.getPattern());
+                                break;
+                            case maxtime:
+                                queryStatement.setInt("time_max", Integer.parseInt(smartInfo.getPattern()));
+                                break;
+                            case mediatype:
+                                queryStatement.setString("mediatype", smartInfo.getPattern());
+                                break;
+                            case protection:
+                                queryStatement.setBoolean("protected", Boolean.parseBoolean(smartInfo.getPattern()));
+                                break;
+                            case videotype:
+                                queryStatement.setString("videotype", smartInfo.getPattern());
+                                break;
+                            case sizeLimit:
+                                queryStatement.setInt("maxCount", Integer.parseInt(smartInfo.getPattern()));
+                                break;
+                            case recentlyPlayed:
+                                queryStatement.setLong("ts_played", System.currentTimeMillis() - (MILLIS_PER_DAY * Long.parseLong(smartInfo.getPattern())));
+                                break;
+                            case recentlyUpdated:
+                                queryStatement.setLong("ts_updated", System.currentTimeMillis() - (MILLIS_PER_DAY * Long.parseLong(smartInfo.getPattern())));
+                                break;
+                            default:
+                                // nothing in all other cases
                         }
-                    });
+                    }
+                    Collection<String> tracks = new LinkedHashSet<>();
+                    DataStoreQuery<QueryResult<String>> dataStoreQuery = new DataStoreQuery<QueryResult<String>>() {
+                        @Override
+                        public QueryResult<String> execute(Connection connection) throws SQLException {
+                            return execute(queryStatement, new ResultBuilder<String>() {
+                                public String create(ResultSet resultSet) throws SQLException {
+                                    return resultSet.getString(1);
+                                }
+                            });
+                        }
+                    };
+                    dataStoreQuery.setFetchOptions(ResultSetType.TYPE_FORWARD_ONLY, 10000);
+                    dataStoreQuery.execute(connection).addRemainingResults(tracks);
+                    SmartStatement statement = MyTunesRssUtils.createStatement(connection, "updateSmartPlaylist");
+                    statement.setString("id", playlistId);
+                    statement.setObject("track_id", tracks);
+                    synchronized (LATCH_MAP) { // synchronize globally to prevent databack locks
+                        statement.execute();
+                    }
+                    return true;
+                } catch (IOException | ParseException e) {
+                    if (LOGGER.isErrorEnabled()) {
+                        LOGGER.error("Could update smart playlist.", e);
+                    }
+                } finally {
+                    LATCH_MAP.get(playlistId).decrementAndGet();
                 }
-            };
-            dataStoreQuery.setFetchOptions(ResultSetType.TYPE_FORWARD_ONLY, 10000);
-            dataStoreQuery.execute(connection).addRemainingResults(tracks);
-            SmartStatement statement = MyTunesRssUtils.createStatement(connection, "updateSmartPlaylist");
-            statement.setString("id", playlistId);
-            statement.setObject("track_id", tracks);
-            synchronized (SYNC) {
-                statement.execute();
-            }
-        } catch (IOException | ParseException e) {
-            if (LOGGER.isErrorEnabled()) {
-                LOGGER.error("Could update smart playlist.", e);
             }
         }
+        return false;
     }
 }
