52853f679a43d634ed26d7464c8cf587721aa8c9
mdescher <mdescher@6cd82e91-2702-0410-863d-86aa3370aca5>
2015-01-12 14:04:48 +0000

    Added first version of the fake itunes library creator.
    git-svn-id: svn://127.0.0.1/mytunesrss/trunk@9119 6cd82e91-2702-0410-863d-86aa3370aca5

---
+++ runtime/src/main/java/de/codewave/mytunesrss/tool/FakeItunesLibraryCreator.java
@@ -0,0 +1,81 @@
+package de.codewave.mytunesrss.tool;
+
+import de.codewave.mytunesrss.datastore.itunes.ItunesLoader;
+import de.codewave.utils.xml.PListHandler;
+import de.codewave.utils.xml.PListHandlerListener;
+import de.codewave.utils.xml.XmlUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
+public class FakeItunesLibraryCreator {
+
+    private static Set<String> missingTemplates = new HashSet<>();
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        final File templateDir = new File(args[0]);
+        File iTunesLibraryXmlFile = new File(args[1]);
+        final String commonPrefix = args[2];
+        URL iTunesLibraryXml = iTunesLibraryXmlFile.toURI().toURL();
+        PListHandler handler = new PListHandler();
+        handler.addListener("/plist/dict[Tracks]/dict", new PListHandlerListener() {
+            @Override
+            public boolean beforeDictPut(Map dict, String key, Object value) {
+                try {
+                    process((Map)value);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+
+            private void process(Map track) throws IOException {
+                String trackType = (String) track.get("Track Type");
+                if (trackType == null || "File".equals(trackType)) {
+                    String filename = ItunesLoader.getFileNameForLocation((String) track.get("Location"));
+                    if (filename.startsWith(commonPrefix)) {
+                        File fakeFile = new File(filename.substring(commonPrefix.length()));
+                        File templateFile = findTemplate(templateDir, filename);
+                        if (templateFile != null && templateFile.isFile() && templateFile.canRead()) {
+                            if (fakeFile.getParentFile().mkdirs()) {
+                                System.out.println("copying \"" + templateFile.getAbsolutePath() + "\" to \"" + fakeFile.getAbsolutePath() + "\".");
+                                FileUtils.copyFile(templateFile, fakeFile);
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public boolean beforeArrayAdd(List list, Object o) {
+                return false;
+            }
+        });
+        XmlUtils.parseApplePList(iTunesLibraryXml, handler);
+        System.out.println("Templates for the following extensions were missing: " + Arrays.toString(missingTemplates.toArray(new String[missingTemplates.size()])));
+    }
+
+    private static File findTemplate(File templateDir, final String filename) {
+        File[] templates = templateDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return FilenameUtils.getExtension(name).equalsIgnoreCase(FilenameUtils.getExtension(filename));
+            }
+        });
+        if (templates.length > 0) {
+            return templates[0];
+        }  else {
+            missingTemplates.add(StringUtils.lowerCase(FilenameUtils.getExtension(filename)));
+            return null;
+        }
+    }
+
+}
